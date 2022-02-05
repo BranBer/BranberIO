2c265fd78ce74ec7cf9d257f5192eb3dd32952f8
branber <brandonberke@gmail.com>
2022-02-05 01:07:40 -0500

    test facebook no mutation

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -54,7 +54,7 @@ export default NextAuth({
                 };
                 break;
               case "facebook":
-                mutation = LOGIN_FACEBOOK;
+                //mutation = LOGIN_FACEBOOK;
                 params = {
                   email: token.email as string,
                   inputToken: token.access_token as string,
@@ -64,13 +64,14 @@ export default NextAuth({
                 break;
             }
 
-            if (mutation) {
-              console.log("MUTATING");
-              let { data } = await client.mutate({
-                mutation: mutation,
-                variables: params,
-              });
-            }
+            // if (mutation) {
+            //   //The issue is here!!!
+            //   console.log("MUTATING");
+            //   let { data } = await client.mutate({
+            //     mutation: mutation,
+            //     variables: params,
+            //   });
+            // }
           }
         }
       }
