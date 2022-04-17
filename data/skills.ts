ec7dcf594db59536cea48512f568a398fc782ef6
branber <brandonberke@gmail.com>
2022-04-17 13:24:49 -0400

    add categories to skills page

--- data/skills.ts
+++ data/skills.ts
@@ -9,95 +9,184 @@ import {
   DiJsBadge,
   DiLinux,
   DiNginx,
-  DiPostgresql,
   DiPython,
   DiReact,
   DiSass,
 } from "react-icons/di";
 import { VscAzure } from "react-icons/vsc";
-import { SiCsharp, SiKubernetes, SiPandas, SiTypescript } from "react-icons/si";
+import {
+  SiAntdesign,
+  SiApollographql,
+  SiCsharp,
+  SiDebian,
+  SiFramer,
+  SiGraphql,
+  SiKubernetes,
+  SiMaterialui,
+  SiMongodb,
+  SiPandas,
+  SiPostgresql,
+  SiRabbitmq,
+  SiRaspberrypi,
+  SiTypescript,
+  SiUbuntu,
+  SiWindows,
+} from "react-icons/si";
+import { AiFillGithub, AiFillPieChart } from "react-icons/ai";
+import { BiGitBranch } from "react-icons/bi";
 
-const skills = [
-  {
-    skill: "Javascript",
-    icon: DiJsBadge,
-  },
-  {
-    skill: "Typescript",
-    icon: SiTypescript,
-  },
-  {
-    skill: "Postgres SQL",
-    icon: DiHtml5,
-  },
-  {
-    skill: "Html5",
-    icon: DiHtml5,
-  },
-  {
-    skill: "CSS3",
-    icon: DiCss3,
-  },
-  {
-    skill: "SASS",
-    icon: DiSass,
-  },
-  {
-    skill: "Django",
-    icon: DiDjango,
-  },
-  {
-    skill: "Python",
-    icon: DiPython,
-  },
-  {
-    skill: "Linux",
-    icon: DiLinux,
-  },
-  {
-    skill: "AWS",
-    icon: DiAws,
-  },
-  {
-    skill: "React.js",
-    icon: DiReact,
-  },
-  {
-    skill: "Docker",
-    icon: DiDocker,
-  },
-  {
-    skill: "Postgres SQL",
-    icon: DiPostgresql,
-  },
-  {
-    skill: "Nginx",
-    icon: DiNginx,
-  },
-  {
-    skill: "Bitbucket",
-    icon: DiBitbucket,
-  },
-  {
-    skill: "Jira",
-    icon: DiJira,
-  },
-  {
-    skill: "Azure",
-    icon: VscAzure,
-  },
-  {
-    skill: "Kubernetes",
-    icon: SiKubernetes,
-  },
-  {
-    skill: "C#",
-    icon: SiCsharp,
-  },
-  {
-    skill: "Pandas",
-    icon: SiPandas,
-  },
-];
+const skills = {
+  Libraries: [
+    {
+      skill: "Material UI",
+      icon: SiMaterialui,
+    },
+    {
+      skill: "Ant Design",
+      icon: SiAntdesign,
+    },
+    {
+      skill: "Framer Motion",
+      icon: SiFramer,
+    },
+    {
+      skill: "Nivo",
+      icon: AiFillPieChart,
+    },
+    {
+      skill: "Pandas",
+      icon: SiPandas,
+    },
+  ],
+  Languages: [
+    {
+      skill: "Javascript",
+      icon: DiJsBadge,
+    },
+    {
+      skill: "Typescript",
+      icon: SiTypescript,
+    },
+
+    {
+      skill: "Html5",
+      icon: SiPostgresql,
+    },
+    {
+      skill: "CSS3",
+      icon: DiCss3,
+    },
+    {
+      skill: "SASS",
+      icon: DiSass,
+    },
+    {
+      skill: "Graphql",
+      icon: SiGraphql,
+    },
+    {
+      skill: "Python",
+      icon: DiPython,
+    },
+    {
+      skill: "C#",
+      icon: SiCsharp,
+    },
+  ],
+  Databases: [
+    {
+      skill: "MongoDB",
+      icon: SiMongodb,
+    },
+    {
+      skill: "Postgres SQL",
+      icon: DiHtml5,
+    },
+  ],
+  Hardware: [
+    {
+      skill: "Raspberry Pi",
+      icon: SiRaspberrypi,
+    },
+  ],
+  Frameworks: [
+    {
+      skill: "Apollo Server",
+      icon: SiApollographql,
+    },
+    {
+      skill: "React.js",
+      icon: DiReact,
+    },
+    {
+      skill: "Django",
+      icon: DiDjango,
+    },
+  ],
+  "Operating Systems": [
+    {
+      skill: "Ubuntu",
+      icon: SiUbuntu,
+    },
+    {
+      skill: "Debian",
+      icon: SiDebian,
+    },
+    {
+      skill: "Windows",
+      icon: SiWindows,
+    },
+  ],
+  "Project Management": [
+    {
+      skill: "Github",
+      icon: AiFillGithub,
+      category: "project",
+    },
+    {
+      skill: "Bitbucket",
+      icon: DiBitbucket,
+      category: "project",
+    },
+    {
+      skill: "Jira",
+      icon: DiJira,
+      category: "project",
+    },
+    {
+      skill: "Git",
+      icon: BiGitBranch,
+      category: "project",
+    },
+  ],
+  Infrastructure: [
+    {
+      skill: "AWS",
+      icon: DiAws,
+    },
+
+    {
+      skill: "Docker",
+      icon: DiDocker,
+    },
+    {
+      skill: "Nginx",
+      icon: DiNginx,
+    },
+
+    {
+      skill: "Azure",
+      icon: VscAzure,
+    },
+    {
+      skill: "Kubernetes",
+      icon: SiKubernetes,
+    },
+    {
+      skill: "RabbitMQ",
+      icon: SiRabbitmq,
+    },
+  ],
+};
 
 export default skills;
--- pages/skills.tsx
+++ pages/skills.tsx
@@ -8,24 +8,42 @@ import { motion } from "framer-motion";
 import variants from "../animations/animations";
 
 const Skills = () => {
+  const skillCategories = Object.entries(skills).sort(
+    ([_, skills1], [__, skills2]) => {
+      return skills2.length - skills1.length;
+    }
+  );
   return (
     <Page>
       <PageHeader text={"Skills"} />
-      <div className={styles.skillsContainer}>
-        {skills.map(({ skill, icon }, index) => (
-          <motion.div
-            key={`skill${index}`}
-            variants={variants}
-            initial="fadeInit"
-            animate="fadeAnimate"
-            transition={{
-              duration: 1,
-              delay: index / (skills.length * 2),
-            }}
-          >
-            <Skill Icon={icon} iconText={skill} />
-          </motion.div>
-        ))}
+
+      <div className={styles.skillCategories}>
+        {skillCategories.map(([category, skills], categoryIndex) => {
+          return (
+            <div
+              key={`skillCategory${categoryIndex}`}
+              className={styles.skillCategory}
+            >
+              <h3>{category}</h3>
+              <div className={styles.skillsContainer}>
+                {skills.map(({ skill, icon }, skillIndex) => (
+                  <motion.div
+                    key={`skill${skillIndex}${categoryIndex}`}
+                    variants={variants}
+                    initial="fadeInit"
+                    animate="fadeAnimate"
+                    transition={{
+                      duration: 1,
+                      delay: skillIndex / (skills.length * 2),
+                    }}
+                  >
+                    <Skill Icon={icon} iconText={skill} />
+                  </motion.div>
+                ))}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </Page>
   );
