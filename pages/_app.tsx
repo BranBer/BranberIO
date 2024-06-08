455a5ca3aece15ac9b5f1421df83d417849419d4
branber <brandonberke@gmail.com>
2024-06-07 23:48:17 -0400

    add page transition css to fix animation issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,27 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import Lavalamp from "../components/lavalamp";
 import { AnimatePresence } from "framer-motion";
 import styles from "../styles/page.module.scss";
 import variants from "../animations/animations";
 import { motion } from "framer-motion";
+import type { AppProps } from "next/app";
+import { useNextCssRemovalPrevention } from "@madeinhaus/nextjs-page-transition";
+import "@madeinhaus/nextjs-page-transition/dist/index.css";
 
 function MyApp({ Component, pageProps: { ...pageProps }, router }: AppProps) {
+  const removeStyles = useNextCssRemovalPrevention();
   return (
     <>
       <div className={styles.background} />
       <Lavalamp />
       <div className={styles.pageContainer}>
         <div className={styles.page}>
-          <AnimatePresence mode="wait">
+          <AnimatePresence
+            mode="wait"
+            onExitComplete={() => {
+              removeStyles();
+            }}
+          >
             <motion.div
               variants={variants}
               initial="fadeInit"
